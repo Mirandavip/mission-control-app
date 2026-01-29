68b938977aea5ac0458376034e74e36da9eb87f2
Tércio Miranda <mirandavip@gmail.com>
2026-01-29 20:59:11 -0300

    Update sw.js
    Atualização de nova versão de logo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'admira-system-v1';
+const CACHE_NAME = 'admira-system-v2';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -46,4 +46,5 @@ self.addEventListener('fetch', (event) => {
         return response || fetch(event.request);
       })
   );
+
 });
